d6b60438c70ff124af676936c72938fe10559f76
craftion <dnzyanik@gmail.com>
2018-11-17 20:26:35 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,39 @@
+var value = 0;
+var babe ;
+var babe2;
 function preload(){
   // put preload code here
   mySong = loadSound("./assets/baby.mp3");
-
+  babe = loadImage("./assets/baby.jpg");
+  babe2 = loadImage("./assets/baby2.jpg");
 }
 
 function setup() {
   // put setup code here
   createCanvas(windowWidth,windowHeight);
+  setMoveThreshold(5);
+  frameRate(12);
+
+
 }
 
 function draw() {
   // put drawing code here
-  if (mouseX > width/2) {
-    background(0,255,0);
-    if (mySong.isPlaying() == false) {
-      mySong.play();
-    }
-  } else {
-    background(255,0,0);
-    mySong.stop();
-  }
+  // if (value>150) {
+  //   image(babe, 0,0,windowWidth,windowHeight);
+  //   // if (mySong.isPlaying() == false) {
+  //   //   mySong.play();
+  //   // }
+  // } else {
+  //
+  //   image(babe2, 0,0,windowWidth,windowHeight);
+  //   // mySong.stop();
+  // }
 
+}
+
+function deviceTurned() {
+  if (turnAxis === 'Z'){
+    mySong.play();
+  }
 }
