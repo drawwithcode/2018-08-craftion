85c45be58db93e6b73f80e3dc79ab370500005d1
craftion <dnzyanik@gmail.com>
2018-11-17 21:39:33 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,35 +1,32 @@
 var value = 0;
-var babe ;
+var babe;
 var babe2;
-function preload(){
+
+function preload() {
   // put preload code here
   mySong = loadSound("./assets/baby.mp3");
-  babe = loadImage("./assets/baby.jpg");
-  babe2 = loadImage("./assets/baby2.jpg");
 }
 
 function setup() {
   // put setup code here
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
+  babe = loadImage("./assets/baby.jpg");
+  babe2 = loadImage("./assets/baby2.jpg");
   setMoveThreshold(5);
   frameRate(12);
-
-
 }
 
 function draw() {
   // put drawing code here
-  if (value>200 && value<100) {
-    image(babe, 0,0,windowWidth,windowHeight);
+  if (value > 200 && value < 100) {
+    image(babe, 0, 0, windowWidth, windowHeight);
     if (mySong.isPlaying() == false) {
       mySong.play();
     }
   } else {
-
-    image(babe2, 0,0,windowWidth,windowHeight);
+    image(babe2, 0, 0, windowWidth, windowHeight);
     mySong.stop();
   }
-
 }
 
 function deviceMoved() {
@@ -37,5 +34,4 @@ function deviceMoved() {
   if (value > 255) {
     value = 0;
   }
-
 }
