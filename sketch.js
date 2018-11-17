ecb5f1044f049604c97361e9f2dde54778821d92
craftion <dnzyanik@gmail.com>
2018-11-17 19:57:47 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,24 @@
 function preload(){
   // put preload code here
+  mySong = loadSound("./assets/baby.mp3");
+
 }
 
 function setup() {
   // put setup code here
+  createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
   // put drawing code here
+  if (mouseX > width/2) {
+    background(0,255,0);
+    if (mySong.isPlaying() == false) {
+      mySong.play();
+    }
+  } else {
+    background(255,0,0);
+    mySong.stop();
+  }
+
 }
